f798113ac8d800a94c14bfa8d19fe67c6df14c8f
RazzDazz <magnus.wilde@hotmail.de>
2017-06-30 18:45:07 +0200

    add create httpswd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@ if [ ! -f "/usr/local/nagios/etc/nagios.cfg" ]
 then
     echo "Copying files for first start to volume" 
     cp -r /usr/local/nagios/etc-ori/* /usr/local/nagios/etc/
+    echo "Create defualt httpasswd. Log into terminal and run: 'htpasswd -bc /usr/local/nagios/etc/htpasswd.users ${NAGIOS_WEBADMIN_USER} <yourpassword>'" 
+    htpasswd -bc /usr/local/nagios/etc/htpasswd.users ${NAGIOS_WEBADMIN_USER} ${NAGIOS_WEBADMIN_START_PASSWORD}
 fi
 
 echo "Starting supervisord ..." 

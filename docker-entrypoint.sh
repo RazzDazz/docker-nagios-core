ce92101ee08b9362fab49a7335bcf0fe6e2a2831
RazzDazz <magnus.wilde@hotmail.de>
2017-07-01 18:10:47 +0200

    fine tuning script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 if [ ! -f "/usr/local/nagios/etc/nagios.cfg" ] 
 then
-    echo "Copying files for first start to volume" 
+    echo "Copying files for first start to volume..." 
     cp -r /usr/local/nagios/etc-ori/* /usr/local/nagios/etc/
-    echo "Create defualt httpasswd. Log into terminal and run: 'htpasswd -bc /usr/local/nagios/etc/htpasswd.users ${NAGIOS_WEBADMIN_USER} <yourpassword>'" 
+    echo "Creating default httpasswd..."
     htpasswd -bc /usr/local/nagios/etc/htpasswd.users ${NAGIOS_WEBADMIN_USER} ${NAGIOS_WEBADMIN_START_PASSWORD}
+    echo "Log into terminal and change default password."
+    echo "Run: 'htpasswd -bc /usr/local/nagios/etc/htpasswd.users ${NAGIOS_WEBADMIN_USER} <yourpassword>'" 
 fi
 
-echo "Starting supervisord ..." 
+echo "Starting supervisord to run nagios ..." 
 exec /usr/bin/supervisord --nodaemon --configuration /tmp/supervisor_nagios.conf
